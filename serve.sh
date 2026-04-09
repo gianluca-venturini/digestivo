0709ce4e0f04d2c20b9f5a386cec2372bfd5b887
Gianluca Venturini <gianluca.91@gmail.com>
2026-04-08 22:50:50 -0700

    Update tailnet port

--- serve.sh
+++ serve.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-tailscale serve --bg http://localhost:3001
+tailscale serve --bg --https=3000 http://localhost:3001
 
 exec NODE_ENV=production bun src/api.ts
